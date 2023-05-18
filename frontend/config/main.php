d9c52d843e0df9f4b41c3dc7d41867524723b688
manpreet <manpreet3166@gmail.com>
2023-05-18 14:21:36 +0530

    Navbar added

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
+    'name'=> 'CampusCare',
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
